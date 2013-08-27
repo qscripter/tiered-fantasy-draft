3b5975926497f8dc64d0708f592bb52524965757
Quinn Scripter <qscripter@gmail.com>
2013-08-27 19:56:55 -0400

    can now delete players from tiers, order of players in tier is now maintained

--- client/views/tierDetail.js
+++ client/views/tierDetail.js
@@ -5,7 +5,11 @@ Template.tierDetail.tier = function () {
 Template.tierDetail.players = function () {
 	var tier = Tiers.findOne(Session.get("selectedTier"));
 	if (tier) {
-		return Players.find({_id: {$in: tier.players}});
+		var players = [];
+		for (var i=0; i < tier.players.length; i++) {
+			players.push(Players.findOne(tier.players[i]));
+		}
+		return players;
 	}
 };
 
@@ -13,3 +17,23 @@ Template.tierDetail.indexOfPlayer = function (playerId) {
 	var tier = Tiers.findOne(Session.get("selectedTier"));
 	return _.indexOf(tier.players, playerId) + 1;
 };
+
+Template.tierDetail.rendered = function () {
+	var players = Players.find().fetch();
+	$('#playerName').typeahead({
+		name: 'playerName',
+		valueKey: 'name',
+		local: players
+	});
+	//$('.tt-query').css('background-color','#fff');
+};
+
+Template.tierDetail.events({
+	'click #addPlayer': function (event) {
+		var playerName = $("#playerName").val();
+		Meteor.call("addPlayerToTier", playerName, Session.get("selectedTier"));
+	},
+	'click .deletePlayer': function (event) {
+		Meteor.call("removePlayerFromTier", this._id, Session.get("selectedTier"));
+	}
+});
