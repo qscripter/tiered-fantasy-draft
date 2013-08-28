23e960e2ac2c8075df9bb3a2c1c45ba9e56cfb3f
Quinn Scripter <qscripter@gmail.com>
2013-08-28 13:54:24 -0400

    proper controls in place to disallow actions and UI from non-admins

--- server/methods.js
+++ server/methods.js
@@ -1,18 +1,22 @@
 Meteor.methods({
 	updatePlayerSalary: function (teamId, playerId, salary) {
-		salary = parseInt(salary, 10);
-		Teams.update({
-			_id: teamId,
-			"roster.player_id": playerId
-		},
-		{$set: {'roster.$.salary': salary}});
+		if (Roles.userIsInRole(this.userId, ['admin'])) {
+			salary = parseInt(salary, 10);
+			Teams.update({
+				_id: teamId,
+				"roster.player_id": playerId
+			},
+			{$set: {'roster.$.salary': salary}});
+		}
 	},
 	updatePlayerContract: function (teamId, playerId, contractYears) {
-		contractYears = parseInt(contractYears, 10);
-		Teams.update({
-			_id: teamId,
-			"roster.player_id": playerId
-		},
-		{$set: {'roster.$.contractYears': contractYears}});
+		if (Roles.userIsInRole(this.userId, ['admin'])) {
+			contractYears = parseInt(contractYears, 10);
+			Teams.update({
+				_id: teamId,
+				"roster.player_id": playerId
+			},
+			{$set: {'roster.$.contractYears': contractYears}});
+		}
 	}
 });
