b822933dfd7ea2d48fe4c860478240ce9987cf2d
QUINN SCRIPTER <qscripter@QUINNs-MacBook-Air.local>
2014-08-27 01:09:50 -0400

    updated chart to pie chart with appropraite colors

--- client/views/positionGraph.js
+++ client/views/positionGraph.js
@@ -1,14 +1,44 @@
+var positions = [
+	{
+		position: 'QB',
+		background: '#428bca',
+		border: '#357ebd'
+	},
+	{
+		position: 'WR',
+		background: '#5cb85c',
+		border: '#4cae4c'
+	},
+	{
+		position: 'RB',
+		background: '#d9534f',
+		border: '#d43f3a'
+	},
+	{
+		position: 'TE',
+		background: '#5bc0de',
+		border: '#46b8da'
+	},
+	{
+		position: 'KK',
+		background: '#f0ad4e',
+		border: '#eea236'
+	},
+	{
+		position: 'ST',
+		background: '#c32e9b',
+		border: '#a32080'
+	}
+]
+
 function buildChartData (team, league) {
 	if (team && league) {
-		var positions = _.map(league.positions, function(item){
-			return item.position;
-		});
-		console.log(positions);
+		salaryTotal = 100;
 		data = []
 		for (var j=0; j < positions.length; j++) {
 			var positionContracts = _.filter(team.roster, function (item) {
 				var player = Players.findOne(item.player_id);
-				return player.position == positions[j];
+				return player.position == positions[j].position;
 			});
 			var contractsTotal = _.reduce(positionContracts, function (memo, contract) {
 				var currentSalary = _.find(contract.salaryAllocation, function (item) {
@@ -16,63 +46,60 @@ function buildChartData (team, league) {
 				})
 				return memo + currentSalary.salary + currentSalary.bonus;
 			}, 0);
-			data.push(contractsTotal);
+			salaryTotal -= contractsTotal;
+			data.push({
+				value: contractsTotal,
+				color: positions[j].background,
+				highlight: positions[j].border,
+				label: positions[j].position
+			});
 		}
+		data.push({
+				value: salaryTotal,
+				color: "#5cb85c",
+				highlight: "#4cae4c",
+				label: "Available"
+			});
 		console.log(data);
-
-		var graphData = {
-		    labels: positions,
-		    datasets: [
-		        {
-		            label: "Position Spending",
-		            fillColor: "rgba(151,187,205,0.5)",
-		            strokeColor: "rgba(151,187,205,0.8)",
-		            highlightFill: "rgba(151,187,205,0.75)",
-		            highlightStroke: "rgba(151,187,205,1)",
-		            data: data
-		        }
-		    ]
-		}
-		return graphData;
+		return data;
 	}
 }
 
 function drawChart(team, league) {
 	var ctx = document.getElementById("myChart").getContext("2d");
 	var options = {
-	    //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-	    scaleBeginAtZero : true,
+		responsive: true,
+	    //Boolean - Whether we should show a stroke on each segment
+	    segmentShowStroke : true,
 
-	    //Boolean - Whether grid lines are shown across the chart
-	    scaleShowGridLines : true,
+	    //String - The colour of each segment stroke
+	    segmentStrokeColor : "#fff",
 
-	    //String - Colour of the grid lines
-	    scaleGridLineColor : "rgba(0,0,0,.05)",
+	    //Number - The width of each segment stroke
+	    segmentStrokeWidth : 2,
 
-	    //Number - Width of the grid lines
-	    scaleGridLineWidth : 1,
+	    //Number - The percentage of the chart that we cut out of the middle
+	    percentageInnerCutout : 0, // This is 0 for Pie charts
 
-	    //Boolean - If there is a stroke on each bar
-	    barShowStroke : true,
+	    //Number - Amount of animation steps
+	    animationSteps : 100,
 
-	    //Number - Pixel width of the bar stroke
-	    barStrokeWidth : 2,
+	    //String - Animation easing effect
+	    animationEasing : "easeOutBounce",
 
-	    //Number - Spacing between each of the X value sets
-	    barValueSpacing : 5,
+	    //Boolean - Whether we animate the rotation of the Doughnut
+	    animateRotate : true,
 
-	    //Number - Spacing between data sets within X values
-	    barDatasetSpacing : 1,
-
-	    responsive: true,
+	    //Boolean - Whether we animate scaling the Doughnut from the centre
+	    animateScale : false,
 
 	    //String - A legend template
-	    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+	    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
 
 	}
 
 	data = buildChartData(team, league);
-	var myBarChart = new Chart(ctx).Bar(data, options);
+	var myPieChart = new Chart(ctx).Pie(data,options);
 }
 
 
